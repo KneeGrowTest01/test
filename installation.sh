2b7820ffa9beb19ed47522bc1011a6df3cabb3c5
KneeGrowTest01 <KneeGrowTest01@gmail.com>
2020-02-28 14:30:58 +0800

    Another commit

--- installation.sh
+++ installation.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ssh -t -o StrictHostKeyChecking=no russel.sarzadilla@34.94.225.78
+whoami
 sudo yum group install "Development Tools"
 sudo yum install tuned gcc gcc-c++ bzip2 tar unzip gzip curl fileutils binutils iproute pam-devel whois mtr iftop libxml2-devel libxslt-devel tcptraceroute python dhcp zlib-devel openssl-devel readline-devel ncurses-devel wget tar dnsmasq net-tools iptables-services system-config-network-tui system-config-firewall-tui nano iptables-services openssh openssh-server openssh-clients openssl-libs net-tools wget zip unzip git mlocate screen vim nc -y
 sudo tuned-adm profile latency-performance
