4b5c3abfb3bb9b7bc562a34ca0d28694cc9d6524
KneeGrowTest01 <53508703+KneeGrowTest01@users.noreply.github.com>
2020-03-06 04:56:42 +0800

    Delete installation.sh

--- installation.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-ssh -t -o StrictHostKeyChecking=no russel.sarzadilla@34.94.225.78
-whoami
-sudo yum group install "Development Tools"
-sudo yum install tuned gcc gcc-c++ bzip2 tar unzip gzip curl fileutils binutils iproute pam-devel whois mtr iftop libxml2-devel libxslt-devel tcptraceroute python dhcp zlib-devel openssl-devel readline-devel ncurses-devel wget tar dnsmasq net-tools iptables-services system-config-network-tui system-config-firewall-tui nano iptables-services openssh openssh-server openssh-clients openssl-libs net-tools wget zip unzip git mlocate screen vim nc -y
-sudo tuned-adm profile latency-performance
-sudo yum update -y
-sudo sed -i 's/SELINUX=disabled/SELINUX=enabled/g' /etc/selinux/config
-sudo cat /etc/selinux/config
